ba665b4db175112183065b85a4978308b71ad203
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-01-15 12:15:15 -0500

    Add shibe command. Very doge, much yes.

---
+++ src/commands/shibe.js
@@ -0,0 +1,22 @@
+const request = require('superagent')
+
+module.exports = {
+  meta: {
+    help: 'Get a random shibe picture.',
+    module: 'Fun',
+    level: 0,
+    timeout: 10,
+    alias: ['shiba', 'doge', 'shibainu']
+  },
+  fn: function (msg) {
+    request.get('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true')
+      .end((err, res) => {
+        if (!err && res.status === 200) {
+          msg.channel.createMessage(res.body[0])
+        } else {
+          global.i18n.send('API_ERROR', msg.channel)
+          global.logger.error(`REST call failed: ${err}`)
+        }
+      })
+  }
+}
