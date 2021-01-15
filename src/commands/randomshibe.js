00fcfa7c06dbb5374fbc199cf071e6ebdaca9b67
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-01-15 12:16:04 -0500

    Change filename and add shibe as alias

--- src/commands/randomshibe.js
+++ src/commands/randomshibe.js
@@ -6,7 +6,7 @@ module.exports = {
     module: 'Fun',
     level: 0,
     timeout: 10,
-    alias: ['shiba', 'doge', 'shibainu']
+    alias: ['shiba', 'doge', 'shibainu', 'shibe']
   },
   fn: function (msg) {
     request.get('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true')
