e8c8b88f2a0aa7ad0eb2d72f9bf06f1e397e7126
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-01-16 01:20:52 -0500

    Fix pikachu.js because I made a dumb mistake

--- src/commands/pikachu.js
+++ src/commands/pikachu.js
@@ -12,7 +12,7 @@ module.exports = {
     request.get('https://some-random-api.ml/img/pikachu')
       .end((err, res) => {
         if (!err && res.status === 200) {
-          msg.channel.createMessage(res.body.image)
+          msg.channel.createMessage(res.body.link)
         } else {
           global.i18n.send('API_ERROR', msg.channel)
           global.logger.error(`REST call failed: ${err}`)
