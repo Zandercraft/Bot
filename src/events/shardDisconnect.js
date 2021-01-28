8da8ba76d1d7088f4e94d1f8163aa28c3cc4fa7e
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-01-28 14:39:10 -0500

    Uncaught Error

--- src/events/shardDisconnect.js
+++ src/events/shardDisconnect.js
@@ -1,4 +1,5 @@
 module.exports = function (ctx) {
   if (ctx[0] !== null && ctx[0].message === 'Authentication failed') global.logger.error('Your token is incorrect, authentication failed', true)
+  else if (ctx[0] !== null) global.logger.warn(ctx[0])
   else (ctx[0] === null) ? global.logger.warn('Discord connection broke, shard disconnected') : global.logger.warn(ctx[0])
 }
