cafccff6c917ad8f45485f500127210df16fc9f9
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-01-28 14:05:03 -0500

    Fix linting error

--- src/commands/InTheWorksAndDisabledCommands/lock.js
+++ src/commands/InTheWorksAndDisabledCommands/lock.js
@@ -16,7 +16,7 @@ module.exports = {
           channel.editPermission(channel.guild.id, channel.permissionOverwrites.filter(id => id.id == channel.guild.id)[0].allow, 2048, 'role', 'Channel Lock')
         })
       } elseif (suffix[0] == 'off') {
-        
+        channel.editPermission(channel.guild.id, channel.permissionOverwrites.filter(id => id.id == channel.guild.id)[0].allow, 2048, 'role', 'Channel Unlock') // RIP. Fix linting error
       }
     } else {
       msg.channel.createMessage('Please provide either "on" or "off" after the lock command.');
